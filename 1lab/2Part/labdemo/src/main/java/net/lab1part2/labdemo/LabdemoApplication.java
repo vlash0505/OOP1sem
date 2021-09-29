73f2d81f6246c42bf9d5c09ca5c41473f1608083
Vladyslav Shupliakov <shupliakovvl@gmail.com>
2021-09-29 23:15:00 +0300

    Initialized Spring project, created database on MySQL server, created table that stores info about books.

---
+++ 1lab/2Part/labdemo/src/main/java/net/lab1part2/labdemo/LabdemoApplication.java
@@ -0,0 +1,13 @@
+package net.lab1part2.labdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdemoApplication.class, args);
+	}
+
+}
---
+++ 1lab/2Part/labdemo/src/test/java/net/lab1part2/labdemo/LabdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lab1part2.labdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
