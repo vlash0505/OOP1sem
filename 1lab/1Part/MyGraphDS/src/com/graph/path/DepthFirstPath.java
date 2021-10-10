15a737d0ed49ff3bde1d8639c55f8d0f904e29af
Vladyslav Shupliakov <shupliakovvl@gmail.com>
2021-10-10 13:31:54 +0300

    Adapted dfs for generics

--- 1lab/1Part/MyGraphDS/src/com/graph/path/DepthFirstPath.java
+++ 1lab/1Part/MyGraphDS/src/com/graph/path/DepthFirstPath.java
@@ -1,6 +1,9 @@
 package com.graph.path;
 
 import com.graph.implementation.GraphAdjList;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -10,19 +13,19 @@ import java.util.Stack;
 
 public class DepthFirstPath<T> {
     private boolean[] isVisited;
-    private int[] edgeTo;
+    private List<T> edgeTo;
     private final int s;
 
     /**
      * Constructor for pathfinding using DFS.
      *
-     * @param G - given graph.
-     * @param s - starting point
+     * @param G  given graph.
+     * @param s  starting point
      */
 
     public DepthFirstPath(GraphAdjList<T> G, int s) {
         this.isVisited = new boolean[G.V()];
-        this.edgeTo = new int[G.V()];
+        this.edgeTo = new ArrayList<>();
         this.s = s;
         traverse(G, s);
     }
@@ -30,45 +33,55 @@ public class DepthFirstPath<T> {
     /**
      * Recursive DFS method to traverse the graph.
      *
-     * @param G - given graph
-     * @param v - vertex that is being inspected.
+     * @param G  given graph
+     * @param v  index of a vertex that is being inspected.
      */
 
     public void traverse(GraphAdjList<T> G, int v) {
         isVisited[v] = true;
-        for (int w : G.getVertices().get(v))
-            if (!isVisited[w]) {
-                edgeTo[w] = v;
-                traverse(G, w);
+        T toInspect = G.getIndexedVertices().get(v);
+        //going through adjacent vertices to the vertex
+        //we are currently inspecting.
+        for (T w : G.getVertices().get(toInspect)) {
+            int index = G.getIndexedVertices().indexOf(w);
+            if (!isVisited[index]) {
+                edgeTo.add(w);
+                traverse(G, index);
             }
+        }
     }
 
     /**
-     * Method that checks whether the path from source to
-     * the given vertex exists.
+     * Method that checks whether the path to the given
+     * vertex exists.
      *
-     * @param v - vertex to check
+     * @param G       Given graph.
+     * @param element Vertex that we want to check the path exists
+     *                to from the source vertex.
      *
      * @return true if the path exists, otherwise - false.
      */
 
-    public boolean hasPathTo(int v) { return isVisited[v]; }
+    public boolean hasPathTo(GraphAdjList<T> G, T element) {
+        return isVisited[G.getIndexedVertices().indexOf(element)];
+    }
 
     /**
      * Method that researches traversed graph and
      * builds a path to the given vertex(if it exists).
      *
-     * @param v - vertex to be reached.
+     * @param G       Given graph.
+     * @param element Vertex that we want to get path to
+     *                from the source vertex.
      *
      * @return path to the vertex v from the source vertex
      * as the Iterable.
      */
 
-    public Iterable<Integer> pathTo(int v) {
-        if(!hasPathTo(v)) { return null; }
-        Stack<Integer> path = new Stack<>();
-        for (int x = v; x != s; x = edgeTo[x]) { path.push(x); }
-        path.push(s);
+    public Iterable<T> pathTo(GraphAdjList<T> G, T element) {
+        if(!hasPathTo(G, element)) { return null; }
+        Stack<T> path = new Stack<>();
+        for (T x : edgeTo) { path.push(x); }
         return path;
     }
 }
