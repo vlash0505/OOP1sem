2981c5edfa9fcc533710c8fcbec1cc8cacfb8c92
Vladyslav Shupliakov <shupliakovvl@gmail.com>
2021-10-10 13:06:31 +0300

    Small bug fixes and changes to documentation

--- 1lab/1Part/MyGraphDS/src/com/graph/path/BreadthFirstPath.java
+++ 1lab/1Part/MyGraphDS/src/com/graph/path/BreadthFirstPath.java
@@ -39,13 +39,14 @@ public class BreadthFirstPath<T> {
     public void traverse(GraphAdjList<T> G, T s) {
         //Using Queue interface and Linked List class
         //to implement queue.
-        int currentNodeIndex = G.getIndexedVertices().indexOf(s);
+        int currentIndex = G.getIndexedVertices().indexOf(s);
         Queue<Integer> queue = new LinkedList<>();
-        isVisited[currentNodeIndex] = true;
-        queue.offer(currentNodeIndex);
+        isVisited[currentIndex] = true;
+        queue.offer(currentIndex);
         while (!queue.isEmpty()) {
             int v = queue.remove();
-            for (T w : G.getVertices().get(s)) {
+            T toInspect = G.getIndexedVertices().get(v);
+            for (T w : G.getVertices().get(toInspect)) {
                 int index = G.getIndexedVertices().indexOf(w);
                 if (!isVisited[index]) {
                     edgeTo.add(w);
@@ -60,7 +61,9 @@ public class BreadthFirstPath<T> {
      * Method that checks whether the path to the given
      * vertex exists.
      *
-     * @param v - vertex to check.
+     * @param G       Given graph.
+     * @param element Vertex that we want to check the path exists
+     *                to from the source vertex.
      *
      * @return true if the path exists, otherwise - false.
      */
@@ -73,7 +76,9 @@ public class BreadthFirstPath<T> {
      * Method that researches traversed graph and
      * builds a path to the given vertex(if it exists).
      *
-     * @param v - vertex to be reached.
+     * @param G       Given graph.
+     * @param element Vertex that we want to get path to
+     *                from the source vertex.
      *
      * @return path to the vertex v from the source vertex
      * as the Iterable.
